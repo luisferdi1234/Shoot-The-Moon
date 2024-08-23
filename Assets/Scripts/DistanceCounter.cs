1ae3427172c789fe368ecc9a8ae6d04712f5ff86
JazzStone2 <jazzstone880@gmail.com>
2024-08-22 20:14:39 -0700

    Update DistanceCounter.cs

--- Assets/Scripts/DistanceCounter.cs
+++ Assets/Scripts/DistanceCounter.cs
@@ -5,22 +5,22 @@ using UnityEngine.UI;
 
 public class DistanceCounter : MonoBehaviour
 {
-
+    
     public string DistanceScore;
     private float timer;
     private int distance;
 
     
     void Update()
-    {
+    { //get time from the start of the fly phase
         timer += Time.deltaTime;
-        
+        //converts time to score value
         if (timer > 5f) {
             CoinScore.coinsValue += 5;
             distance += 5;
-
+        //converts int to str
             DistanceScore = distance.ToString();
-
+         //sets start timer to 0
             timer = 0;
         }
         
