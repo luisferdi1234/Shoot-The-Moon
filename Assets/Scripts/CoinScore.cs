81def126cffc995541320a96fa55e5c0ecb2da40
JazzStone2 <jazzstone880@gmail.com>
2024-08-22 20:12:21 -0700

    Update CoinScore.cs

--- Assets/Scripts/CoinScore.cs
+++ Assets/Scripts/CoinScore.cs
@@ -5,18 +5,18 @@ using UnityEngine.UI;
 
 public class CoinScore : MonoBehaviour
 
-{
+{ //set coin value 
     public static int coinsValue = 0;
     Text coins;
 
     void Start()
-    {
+    {  //gets text from screen
         coins = GetComponent<Text> ();
     }
 
     
     void Update()
-    {
+    { //updates coins statis on screen
         coins.text = "Coins" + coinsValue;
     }
 }
