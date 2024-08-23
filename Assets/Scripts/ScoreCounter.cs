892ddb9499eb828a443c152b79f96138d017ecc9
JazzStone2 <jazzstone880@gmail.com>
2024-08-22 20:18:00 -0700

    Update ScoreCounter.cs

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -4,14 +4,16 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class ScoreCounter : MonoBehaviour
-{
+{ 
     public string scoreValueText;
+    //sets start time to 0
     public float scoreValue = 0.0f;
     public float pointIncreasedPerSecond;
 
     void FixedUpdate()
-    {
+    { //updates text on screen
         scoreValueText = ((int)scoreValue).ToString();
+        //increase screen time per second
         scoreValue = pointIncreasedPerSecond * Time.fixedDeltaTime;
     }
 
