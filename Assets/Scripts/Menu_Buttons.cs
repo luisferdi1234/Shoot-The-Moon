2d0d4a7077d53599e9dbd66e1ea4d50b489c03ff
Rashaansmith2022 <113324350+Rashaansmith2022@users.noreply.github.com>
2024-08-26 15:11:32 -0400

    Added a Main Menu

---
+++ Assets/Scripts/Menu_Buttons.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Buttons : MonoBehaviour
+{
+    //method to use to load Scene1
+    public void LoadScene()
+    {
+        //loads the scene1 using the method called LoadScene
+        SceneManager.LoadScene("Scene0");
+    }
+}
