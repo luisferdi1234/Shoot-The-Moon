4c5546b1340250fe41cae3d1c3a28fb6d48e6009
Luis M <lmarti32@uccs.edu>
2024-08-29 11:07:30 -0600

    Added Button Sound to menu, but it's not playing. Also centered options

--- Assets/Scripts/Menu_Buttons.cs
+++ Assets/Scripts/Menu_Buttons.cs
@@ -5,24 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class Menu_Buttons : MonoBehaviour
 {
+    [SerializeField]
+    AudioClip buttonPress;
+
     /// <summary>
     /// Method to use to load Scene0
     /// </summary>
     public void LoadScene()
     {
+        GetComponent<AudioSource>().PlayOneShot(buttonPress);
         //loads the scene1 using the method called LoadScene
         SceneManager.LoadScene("UpgradeStore");
     }
 
     public void LoadOptions()
     {
-        //loads the scene1 using the method called LoadScene
+        GetComponent<AudioSource>().PlayOneShot(buttonPress);
+        //loads the Options Scene
         SceneManager.LoadScene("Options");
     }
 
     public void LoadCredits()
     {
- 
+        GetComponent<AudioSource>().PlayOneShot(buttonPress);
+
+        //Loads the Credits Scene
         SceneManager.LoadScene("Credits");
     }
 
