86e24d2e45d0757b92c277096402eb7575b1e432
JazzStone2 <jazzstone880@gmail.com>
2024-09-13 10:22:54 -0700

    redone stormCloud Movement

--- Assets/Scripts/StormCloud.cs
+++ Assets/Scripts/StormCloud.cs
@@ -1,13 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StormCloud : Enemy
 {
-    protected override void Start()
-    {
-        base.Start();
+    public float distance = 15.0f;
+    private bool movingRight = true;
+    private Vector3 startPosition;
 
-        rb.velocity = Vector2.right * speed;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (Vector3.Distance(startPosition, transform.position) >= distance)
+        {
+            movingRight = !movingRight;
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
+
