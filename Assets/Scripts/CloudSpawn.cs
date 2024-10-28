eda0ecee7867d479e26a2f099718078591c27950
MASON-ZENBOOK\alaba <mason.ferraro1@gmail.com>
2024-10-28 15:01:36 -0400

    build version. Might have fixed the screen size issue?

--- Assets/Scripts/CloudSpawn.cs
+++ Assets/Scripts/CloudSpawn.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using static UnityEditor.FilePathAttribute;
 
 public class CloudSpawn : MonoBehaviour
 {
